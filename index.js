3297ac408c685349a98b889a3025ce439b467c92
Daniel <daniel.sdm.github@gmail.com>
2020-11-08 20:28:33 -0300

    🔖 release: Squid v1.0.0

---
+++ index.js
@@ -0,0 +1,45 @@
+// Dependecies
+const colors = require('colors')
+const prompt = require('prompt-sync')()
+const got = require('got')
+
+const asciiArt = require('./View/ascii')
+
+
+const Main = () => {
+    console.clear()
+    console.log(asciiArt.rainbow)
+    websiteResponsable()
+}
+
+
+const websiteResponsable = async () => {
+    console.log('[#] - Website URL'.gray)
+    const websiteURL = prompt('[#] - Insert the website URL ~> ');
+
+    try {
+        await got(websiteURL)
+        console.log('\n[!] - Connection estabilished. Starting SQUID party!!11\n'.rainbow);
+        gitVulnChecker(websiteURL)
+    } catch (error) {
+        console.log('\n[*] - Connection failure, please verify your URL.'.red);
+    }
+}
+
+
+const gitVulnChecker = async (websiteURL) => {
+
+    console.log('[#] - Git Vulnerability Finder'.gray)
+    const gitURL = websiteURL + '/.git/'
+   
+    try {
+        await got(gitURL)
+        console.log('[!] - .git file found'.green)
+        console.log(`[*] - URL tested: ${gitURL}`.cyan)
+    } catch(error) {
+        return console.log(`[*] - .git file not found, passing test. - URL tested: ${gitURL}`.cyan)
+    }
+
+}
+
+Main()
