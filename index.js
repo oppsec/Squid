03e5f6393a8586e840704a8b3df564a015ec7dec
Daniel <daniel.sdm.github@gmail.com>
2020-11-10 11:27:02 -0300

    ✨ feature: Wordpress Checker

--- index.js
+++ index.js
@@ -9,37 +9,83 @@ const Main = () => {
     console.clear()
     const ascii = fs.readFileSync('./View/ascii.txt', 'utf-8')
     console.log(ascii.toString().rainbow);
-    websiteResponsable()
+    websiteConnection()
 }
 
 
-const websiteResponsable = async () => {
+const websiteConnection = async () => {
     console.log('[#] - Website URL'.gray)
     const websiteURL = prompt('[#] - Insert the website URL ~> ');
 
     try {
         await got(websiteURL)
         console.log('\n[!] - Connection estabilished. Starting SQUID party!!11\n'.rainbow);
-        gitVulnChecker(websiteURL)
+        gitChecker(websiteURL)
     } catch (error) {
         console.log('\n[*] - Connection failure, please verify your URL.'.red);
     }
 }
 
 
-const gitVulnChecker = async (websiteURL) => {
+const gitChecker = async (websiteURL) => {
 
-    console.log('[#] - Git Vulnerability Finder'.gray)
-    const gitURL = websiteURL + '/.git/'
+    console.log('[#] - Git Vulnerability Checker'.gray)
+    const gitURL = `${websiteURL}/.git/`
    
     try {
         await got(gitURL)
         console.log('[!] - .git file found'.green)
         console.log(`[*] - URL tested: ${gitURL}`.cyan)
+        wordpressChecker(websiteURL)
     } catch(error) {
-        return console.log(`[*] - .git file not found, passing test. - URL tested: ${gitURL}`.cyan)
+        console.log(`[*] - .git file not found, passing test. - URL tested: ${gitURL}`.cyan)
+        wordpressChecker(websiteURL)
+    }
+
+    
+
+}
+
+
+const wordpressChecker = async (websiteURL) => {
+
+    console.log('\n[#] - Wordpress Vulnerability Checker'.gray)
+    const checkRobots = `${websiteURL}/robots.txt`
+
+    const request = await got(checkRobots)
+
+    if(request.body.includes('/wp-admin')) {
+        console.log('[!] - Wordpress directory found inside robots.txt'.green); 
+    } else {
+        console.log('[*] - Wordpress directory not found inside robots.txt, passing test'.cyan)
+    }
+       
+    getWordpressInfo(websiteURL)
+
+}
+
+
+const getWordpressInfo = async (websiteURL) => {
+    console.log('[*] - Trying to get information from Wordpress\n'.yellow)
+
+    const wordpressURLs = [
+        `${websiteURL}/wordpress/readme.html`, `${websiteURL}/wordpress/xmlrpc.php`,
+        `${websiteURL}/wordpress/license.txt`, `${websiteURL}/readme.html`,
+        `${websiteURL}/wp-config-sample.php`
+    ]
+
+    const URLs = wordpressURLs.map(url => url).toString()
+
+    try {
+        const response = await got(URLs)
+        console.log(`[!] - Interesting URL found: ${response.url}`.green)
+        websiteInformation(websiteURL)
+    } catch (error) {
+        console.log("[*] - Couldn't get any wordpress information sorry.".red)
+        websiteInformation(websiteURL)
     }
 
 }
+
 
 Main()
